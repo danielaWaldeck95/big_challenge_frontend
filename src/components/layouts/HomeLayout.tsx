e972dcd2d45f4f756a242675464b26f78f59f4ab
danielawaldeck95 <danielawaldeck95@gmail.com>
2023-05-09 11:59:48 -0300

    feat: create a home layout and display it on index view

---
+++ src/components/layouts/HomeLayout.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "../shared/sidebar";
+
+export const HomeLayout = (props: { children: React.ReactNode }) => (
+  <>
+    <Sidebar />
+    <main className="py-10 lg:pl-72">
+      <div className="px-4 sm:px-6 lg:px-8">{props.children}</div>
+    </main>
+  </>
+);
+export default HomeLayout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,9 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Router from "next/router";
-
-import { useStore } from "./store/store";
-
-interface IUser {
-  name: string;
-}
-
-const getUser = async () => {
-  const { token } = useStore.getState();
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  return axios.get<IUser>(`${process.env.NEXT_PUBLIC_API_URL}/user`);
-};
+import HomeLayout from "@/components/layouts/HomeLayout";
 
 export default function Home() {
-  const [user, setUser] = useState<IUser>();
-
-  useEffect(() => {
-    getUser()
-      .then((response) => {
-        setUser(response.data);
-      })
-      .catch(() => {
-        Router.push("/auth");
-      });
-  }, []);
-
   return (
-    <div>{user && <h1 className="text-3xl font-bold underline">Welcome {user.name}!</h1>}</div>
+    <div>
+      <HomeLayout> </HomeLayout>
+    </div>
   );
 }
