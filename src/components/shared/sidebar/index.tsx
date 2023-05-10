b99acda7adafe962ceb1d778ab8192bd947a14ec
danielawaldeck95 <danielawaldeck95@gmail.com>
2023-05-10 13:53:49 -0300

    fix: mutation and context setters

--- src/components/shared/sidebar/index.tsx
+++ src/components/shared/sidebar/index.tsx
@@ -8,69 +8,68 @@ import { useStore } from "@/pages/store/store";
 import NavItem from "./NavItem";
 
 export default function Sidebar() {
-  const { token, user } = useStore();
+  const { setToken, setUser, token, user } = useStore();
   const [selected, setSelected] = useState("Menu");
 
-  const mutationLogout = useMutation(() => logout(token), {
+  const { mutate } = useMutation({
+    mutationFn: logout,
     onSuccess: () => {
-      useStore.getState().setToken("");
-      useStore.getState().setUser(undefined);
+      setToken("");
+      setUser(undefined);
       Router.push("/auth");
     },
   });
 
   return (
-    <>
-      <div className="fixed inset-y-0 z-50 flex w-72 flex-col">
-        <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-gray-800 px-6 text-gray-300">
-          <nav className="flex flex-1 flex-col pt-10">
-            <ul role="list" className="flex flex-1 flex-col gap-y-2">
+    <div className="fixed inset-y-0 z-50 flex w-72 flex-col">
+      <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-gray-800 px-6 text-gray-300">
+        <nav className="flex flex-1 flex-col pt-10">
+          <ul role="list" className="flex flex-1 flex-col gap-y-2">
+            <NavItem
+              name="Menu"
+              href="/#"
+              navIcon={HomeIcon}
+              selected={selected === "Menu"}
+              onClick={() => setSelected("Menu")}
+            />
+            {user && user.role.includes("PATIENT") && (
               <NavItem
-                name="Menu"
+                name="New Submission"
                 href="/#"
-                navIcon={HomeIcon}
-                selected={selected === "Menu"}
-                onClick={() => setSelected("Menu")}
+                navIcon={PlusCircleIcon}
+                selected={selected === "New Submission"}
+                onClick={() => setSelected("New Submission")}
               />
-              {user && user.role.includes("PATIENT") && (
-                <NavItem
-                  name="New Submission"
-                  href="/#"
-                  navIcon={PlusCircleIcon}
-                  selected={selected === "New Submission"}
-                  onClick={() => setSelected("New Submission")}
-                />
-              )}
-              {user && user.role.includes("DOCTOR") && (
-                <NavItem
-                  name="Task History"
-                  href="/#"
-                  navIcon={InboxIcon}
-                  selected={selected === "Task History"}
-                  onClick={() => setSelected("Task History")}
-                />
-              )}
+            )}
+            {user && user.role.includes("DOCTOR") && (
+              <NavItem
+                name="Task History"
+                href="/#"
+                navIcon={InboxIcon}
+                selected={selected === "Task History"}
+                onClick={() => setSelected("Task History")}
+              />
+            )}
 
-              <li className="-mx-6 mt-auto bg-gray-700 text-white">
-                <div className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6">
-                  <div className="relative flex h-12 w-12 items-center justify-center rounded-full bg-gray-400 p-3">
-                    <span>{user?.name.charAt(0).toUpperCase()}</span>
-                  </div>
-                  <div className="flex flex-col">
-                    <span aria-hidden="true">{user?.name}</span>
-                    <span
-                      className="cursor-pointer text-xs text-gray-300"
-                      onClick={() => mutationLogout.mutate()}
-                    >
-                      Sign out
-                    </span>
-                  </div>
+            <li className="-mx-6 mt-auto bg-gray-700 text-white">
+              <div className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6">
+                <div className="relative flex h-12 w-12 items-center justify-center rounded-full bg-gray-400 p-3">
+                  <span>{user?.name.charAt(0).toUpperCase()}</span>
                 </div>
-              </li>
-            </ul>
-          </nav>
-        </div>
+                <div className="flex flex-col">
+                  <span aria-hidden="true">{user?.name}</span>
+                  <span
+                    className="cursor-pointer text-xs text-gray-300"
+                    onClick={() => mutate(token)}
+                  >
+                    Sign out
+                  </span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </>
+    </div>
   );
 }
